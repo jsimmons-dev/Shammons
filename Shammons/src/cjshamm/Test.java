7b28d0f7b319dde917e3684e394da4764a46efb5
Borimir <lotrfan65@yahoo.com>
2013-05-22 15:49:31 -0400

    Display attempt

--- Shammons/src/cjshamm/Test.java
+++ Shammons/src/cjshamm/Test.java
@@ -1,13 +1,25 @@
 package cjshamm;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Test {
 
 	/**
 	 * @param args
+	 * @throws LWJGLException 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+//		try {
+//			Display.setDisplayMode(new DisplayMode(400,600));
+//			Display.create();
+//		} catch (LWJGLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+}
 
 }
